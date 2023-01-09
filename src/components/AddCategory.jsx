14fa758c3cc01319b68acfb868a00d34c627aed5
Federico Palmieri <fedepalmie95@gmail.com>
2023-01-09 17:20:43 -0300

    feat - gif app project

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export const AddCategory = ({ onNewValue }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const onInputChange = ({ target }) => {
+    //recibe del event el target
+    setInputValue(target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim().length <= 1) return;
+
+    onNewValue(inputValue.trim());
+    setInputValue('');
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Buscar gifs"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
